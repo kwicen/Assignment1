ab533ed7241f80a7630d087444cf70a3a1c2213c
Kelsey W <kwicento@ualberta.ca>
2014-01-28 19:46:31 -0700

    added 2 new classes to handle the individual counters and counts associated with them

---
+++ kwicento-Count/src/com/example/kwicento_count/Counter.java
@@ -0,0 +1,50 @@
+package com.example.kwicento_count;
+
+import java.util.ArrayList;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Counter {
+	
+	protected Button countButton;
+	protected int count;
+	protected ArrayList <Counts> lst;
+	
+	
+	public Button getCountButton() {
+		return countButton;
+	}
+	
+	public void setCountButton(Button countButton) {
+		this.countButton = countButton;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	public ArrayList<Counts> getLst() {
+		return lst;
+	}
+	
+	public void setLst(ArrayList<Counts> lst) {
+		this.lst = lst;
+	}
+	
+	public void incrementCount(View v)
+	{
+		(this.count)++;
+	}
+	
+	public void reset(View v)
+	{
+		(this.count) = 0;
+		//ArrayList <Counts> newlst = new ArrayList;
+		//(this.lst) = (new ArrayList <Counts> newlst);
+	}
+}
---
+++ kwicento-Count/src/com/example/kwicento_count/Counts.java
@@ -0,0 +1,5 @@
+package com.example.kwicento_count;
+
+public class Counts {
+
+}
