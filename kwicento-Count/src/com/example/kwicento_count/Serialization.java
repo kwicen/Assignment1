34a4fdd35cdc467d60dd1c29d072f3983eeec3d8
Kelsey W <kwicento@ualberta.ca>
2014-01-30 21:42:24 -0700

    Finshed saving in serializable, tested and still runs

--- kwicento-Count/src/com/example/kwicento_count/Serialization.java
+++ kwicento-Count/src/com/example/kwicento_count/Serialization.java
@@ -3,22 +3,29 @@ package com.example.kwicento_count;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import android.content.Context;
 
-public class Serialization {
+public class Serialization implements Serializable{
 
+	private static final long serialVersionUID = 1L;
 	protected static final String SAVLOCATION = "file.sav";
 	protected Context fileContext;
 	
+	//takes in the ArrayList storing the counters and writes it to a file
 	protected void saving(ArrayList<Counter> countLst)
 	{
 		try {
 			FileOutputStream fos = fileContext.getApplicationContext().openFileOutput(SAVLOCATION, 
 					Context.MODE_APPEND);
-			fos.write
-			fos.close();
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(countLst);
+			oos.flush();
+            oos.close();
+            fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
