3e7c56bd2e918534b0473397005911c07ea95ec8
Kelsey W <kwicento@ualberta.ca>
2014-01-30 22:49:50 -0700

    renamed a CounterList class. Created attribute currentCounters here that will be used in all files

--- kwicento-Count/src/com/example/kwicento_count/CounterList.java
+++
@@ -1,5 +0,0 @@
-package com.example.kwicento_count;
-
-public class CounterList {
-
-}
---
+++ kwicento-Count/src/com/example/kwicento_count/CounterListModel.java
@@ -0,0 +1,20 @@
+package com.example.kwicento_count;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class CounterListModel implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	protected ArrayList<Counter> currentCounters;
+	
+	
+	public ArrayList<Counter> getCurrentCounters() {
+		return currentCounters;
+	}
+	public void setCurrentCounters(ArrayList<Counter> currentCounters) {
+		this.currentCounters = currentCounters;
+	}
+			
+	
+}
