87e3e2d424593ca9bf23672e7e99c89c79c2b520
Kelsey W <kwicento@ualberta.ca>
2014-01-31 02:53:18 -0700

    formatted the uml for the screen that will do the actual counting

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Assignment1
 
 Assignment 1 for CMPUT 301
 
+The name of my application is kwicento-Count.
+
 License is included in file LICENSE.
--- kwicento-Count/gen/com/example/kwicento_count/R.java
+++ kwicento-Count/gen/com/example/kwicento_count/R.java
@@ -24,10 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080009;
         public static final int button1=0x7f080003;
+        public static final int button2=0x7f080007;
+        public static final int button3=0x7f080006;
         public static final int currCounterList=0x7f080000;
         public static final int enter_name=0x7f080004;
+        public static final int fillName=0x7f080005;
+        public static final int stat=0x7f080008;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080002;
     }
@@ -35,7 +39,8 @@ public final class R {
         public static final int activity_counter_list_screen=0x7f030000;
         public static final int activity_counter_screen=0x7f030001;
         public static final int activity_creating_new_button=0x7f030002;
-        public static final int list_item=0x7f030003;
+        public static final int actual_counting_screen=0x7f030003;
+        public static final int list_item=0x7f030004;
     }
     public static final class menu {
         public static final int counter_list_screen=0x7f070000;
@@ -46,12 +51,18 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int buttonCreate=0x7f050003;
+        public static final int counterTotal=0x7f05000e;
+        public static final int delete=0x7f050010;
         public static final int enterNameText=0x7f050005;
         public static final int enter_name=0x7f050006;
         public static final int hello_world=0x7f050002;
         public static final int listButtonText=0x7f05000a;
+        public static final int nameOfCounter=0x7f05000d;
+        public static final int reset=0x7f05000f;
         public static final int saveNew=0x7f050007;
+        public static final int stats=0x7f050011;
         public static final int title_activity_counter_list_screen=0x7f05000b;
+        public static final int title_activity_counter_screen=0x7f05000c;
         public static final int title_activity_creating_new_button=0x7f050004;
         public static final int welcome1=0x7f050008;
         public static final int welcome2=0x7f050009;
